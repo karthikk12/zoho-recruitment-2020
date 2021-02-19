40534a954c4c18ca170ada2ecaa8d1df6cbabac3
karthikk12 <49509746+karthikk12@users.noreply.github.com>
2021-02-19 09:52:42 +0530

    Update Karthikeyan_Zoho_Test1.java

--- Karthikeyan_Zoho_Test1.java
+++ Karthikeyan_Zoho_Test1.java
@@ -36,15 +36,21 @@ public class Test1 {
 
 		for (int i = 0; i <= validString.length(); i++) {
 
-			System.out.println("");
+			int cnt = 0;
+
+			for (int j = 7; j > i; j--) {
+
+				cnt++;
+				System.out.print(" ");
+
+			}
 
 			for (int j = 0; j < i; j++) {
 
-				System.out.print(validString.charAt(j) + " ");
+				System.out.print(validString.charAt(j));
 
 			}
-			System.out.print("");
-
+			System.out.println("");
 		}
 
 	}
